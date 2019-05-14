6301f7b121a8a9c0c86dba5ba6f4dc5ae298a394
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-05-15 02:40:49 +0500

    classes

---
+++ Foodix/constr.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodix
+{
+    static class constr
+    {
+        public static string co = "Data Source=TAYYAB\\SQLEXPRESS;Initial Catalog=foodix;User ID=sa;Password=1234";
+    }
+}
---
+++ Foodix/customerdata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodix
+{
+    static class customerdata
+    {
+        public static string name;
+        public static string cell;
+        public static string cnic;
+        public static string email;
+        public static string ucell;
+        public static string ch = "0";
+    }
+}
